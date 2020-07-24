756fb32c670afc84742958e20623f807edac4a0e
Federico Jinich <jinichfede@gmail.com>
2020-07-24 02:26:49 -0300

    connect to rskregtest

--- truffle-config.js
+++ truffle-config.js
@@ -18,9 +18,7 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -58,16 +56,11 @@ module.exports = {
       // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    regtest: {
+      provider: () => new HDWalletProvider("c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4", "http://localhost:4444"),
+      network_id: 33,       // Ropsten's id
+      from: "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826"
+    },
 
     // Useful for private networks
     // private: {

2ba0e8d52542a077821c8db973a4129aff7d7ecf
fedejinich <federico@iovlabs.org>
2021-03-30 16:51:10 -0300

    prec

--- contracts/PrecompiledContract.sol
+++ contracts/PrecompiledContract.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0;
 
-contract PrecompiledContractsUtil {
+contract PrecompiledContract {
     event PrecompiledSuccess(address);
     event PrecompiledFailure(address);
 
@@ -29,6 +29,34 @@ contract PrecompiledContractsUtil {
             emit PrecompiledFailure(precAddress);
 
             return false;   
+        }
+    }
+
+    function callPrecTest(address precAddress) public returns (bool) {
+        bytes32 input;
+        uint256 inputLen = 32;
+        uint256 outLen = 64;
+
+        uint256 retval;
+
+        assembly {
+            // allocate output byte array
+            let res := mload(0x40)
+
+            // call precompile (STATICCALL returns success or failure)
+            retval := staticcall(gas(), precAddress, input, inputLen, res, outLen)
+        }
+
+        if(retval == 1) {
+            emit PrecompiledSuccess(precAddress);
+
+            return true;
+        }
+
+        if(retval == 0) {
+            emit PrecompiledFailure(precAddress);
+
+            return false;
         }
     }
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,11 @@
 const Migrations = artifacts.require("Migrations");
 const Blake2b = artifacts.require("Blake2b");
 const Altbn128 = artifacts.require("Altbn128");
+const PrecompiledContract = artifacts.require("PrecompiledContract");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Blake2b);
   deployer.deploy(Altbn128);
+  deployer.deploy(PrecompiledContract);
 };
